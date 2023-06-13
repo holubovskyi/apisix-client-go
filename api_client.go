2e5e78b9541e207d03edf9b0e430d79600724364
Mykhailo Holubovskyi <mholubovskyi@scalhive.com>
2023-06-13 12:13:45 +0300

    Update CreateSslCertificate func

--- api_client.go
+++ api_client.go
@@ -7,7 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-//	"time"
+	// "time"
 )
 
 type AddHeadersRoundtripper struct {
--- api_client_ssl_certificate.go
+++ api_client_ssl_certificate.go
@@ -2,6 +2,7 @@ package api_client
 
 import (
 	"encoding/json"
+	"strings"
 	//	"errors"
 	"fmt"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 
 type SSLCertificate struct {
 	ID          string            `json:"id"`
+	Status      uint              `json:"status"`
 	Certificate string            `json:"cert"`
 	PrivateKey  string            `json:"key"`
 	SNIs        []string          `json:"snis"`
@@ -31,17 +33,42 @@ func (c *ApiClient) GetSslCertificate(certificateID string, apiKey *string) (*SS
 		return nil, err
 	}
 
-	sslCertificate := SSLCertificate{}
-	err = json.Unmarshal(body, &sslCertificate)
+	certificate := SSLCertificate{}
+	err = json.Unmarshal(body, &certificate)
 	if err != nil {
 		return nil, err
 	}
 
-	return &sslCertificate, nil
+	return &certificate, nil
 }
 
-func (client ApiClient) CreateSslCertificate(data map[string]interface{}) (map[string]interface{}, error) {
-	return client.RunObject("POST", "/apisix/admin/ssls/", &data)
+// func (client ApiClient) CreateSslCertificate(data map[string]interface{}) (map[string]interface{}, error) {
+// 	return client.RunObject("POST", "/apisix/admin/ssls/", &data)
+// }
+
+func (c *ApiClient) CreateSslCertificate(sslCertificate SSLCertificate, apiKey *string) (*SSLCertificate, error) {
+	rb, err := json.Marshal(sslCertificate)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s//apisix/admin/ssls/", c.Endpoint), strings.NewReader(string(rb)))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.doRequest(req, apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	certificate := SSLCertificate{}
+	err = json.Unmarshal(body, &certificate)
+	if err != nil {
+		return nil, err
+	}
+
+	return &certificate, nil
 }
 
 func (client ApiClient) UpdateSslCertificate(id string, data map[string]interface{}) (map[string]interface{}, error) {
