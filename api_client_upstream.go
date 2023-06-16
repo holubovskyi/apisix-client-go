b829ecdc50fd2e53038b8c3ec6aa8b4a50ff520d
Mykhailo Holubovskyi <mholubovskyi@scalhive.com>
2023-06-16 11:35:26 +0300

    Update upstream to correspond to the provider model

--- api_client_upstream.go
+++ api_client_upstream.go
@@ -9,25 +9,25 @@ import (
 )
 
 type Upstream struct {
-	ID              string                    `json:"id,omitempty"`
-	Type            string                    `json:"type"`
-	ServiceName     string                    `json:"service_name,omitempty"`
-	DiscoveryType   string                    `json:"discovery_type,omitempty"`
-	Timeout         *TimeoutType              `json:"timeout,omitempty"`
-	Name            string                    `json:"name,omitempty"`
-	Desc            string                    `json:"desc,omitempty"`
-	PassHost        string                    `json:"pass_host,omitempty"`
-	Scheme          string                    `json:"scheme"`
-	Retries         uint                      `json:"retries,omitempty"`
-	RetryTimeout    uint                      `json:"retry_timeout,omitempty"`
-	Labels          map[string]string         `json:"labels,omitempty"`
-	UpstreamHost    string                    `json:"upstream_host,omitempty"`
-	HashOn          string                    `json:"hash_on,omitempty"`
-	Key             string                    `json:"key,omitempty"`
-	KeepalivePool   UpstreamKeepAlivePoolType `json:"keepalive_pool,omitempty"`
-	TLSClientCertID string                    `json:"tls.client_cert_id,omitempty"`
-	Checks          UpstreamChecksType        `json:"checks,omitempty"`
-	Nodes           []UpstreamNodeType        `json:"nodes,omitempty"`
+	ID              string                     `json:"id,omitempty"`
+	Type            string                     `json:"type"`
+	ServiceName     string                     `json:"service_name,omitempty"`
+	DiscoveryType   string                     `json:"discovery_type,omitempty"`
+	Timeout         *TimeoutType               `json:"timeout,omitempty"`
+	Name            string                     `json:"name,omitempty"`
+	Desc            string                     `json:"desc,omitempty"`
+	PassHost        string                     `json:"pass_host,omitempty"`
+	Scheme          string                     `json:"scheme"`
+	Retries         uint                       `json:"retries,omitempty"`
+	RetryTimeout    uint                       `json:"retry_timeout,omitempty"`
+	Labels          map[string]string          `json:"labels,omitempty"`
+	UpstreamHost    string                     `json:"upstream_host,omitempty"`
+	HashOn          string                     `json:"hash_on,omitempty"`
+	Key             string                     `json:"key,omitempty"`
+	KeepalivePool   *UpstreamKeepAlivePoolType `json:"keepalive_pool,omitempty"`
+	TLSClientCertID string                     `json:"tls.client_cert_id,omitempty"`
+	Checks          *UpstreamChecksType        `json:"checks,omitempty"`
+	Nodes           *[]UpstreamNodeType        `json:"nodes,omitempty"`
 }
 
 type TimeoutType struct {
