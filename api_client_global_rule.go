2404aabbc88e3465836a696075cc9defa06c8f34
Mykhailo Holubovskyi <mholubovskyi@scalhive.com>
2023-06-22 18:04:39 +0300

    Implement global rule

--- api_client_global_rule.go
+++ api_client_global_rule.go
@@ -1,17 +1,119 @@
 package api_client
 
-func (client ApiClient) GetGlobalRule(id string) (map[string]interface{}, error) {
-	return client.RunObject("GET", "/apisix/admin/global_rules/"+id, nil)
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type GlobalRule struct {
+	ID      *string                 `json:"id"`
+	Plugins *map[string]interface{} `json:"plugins"`
 }
 
-func (client ApiClient) CreateGlobalRule(id string, data map[string]interface{}) (map[string]interface{}, error) {
-	return client.RunObject("PUT", "/apisix/admin/global_rules/"+id, &data)
+type GlobalRuleResponse struct {
+	Key   string     `json:"key"`
+	Value GlobalRule `json:"value"`
 }
 
-func (client ApiClient) UpdateGlobalRule(id string, data map[string]interface{}) (map[string]interface{}, error) {
-	return client.RunObject("PATCH", "/apisix/admin/global_rules/"+id, &data)
+// GetGlobalRule - Returns a specific global rule
+func (c *ApiClient) GetGlobalRule(ruleID string) (*GlobalRule, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/apisix/admin/global_rules/%s", c.Endpoint, ruleID), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	getResponse := GlobalRuleResponse{}
+	err = json.Unmarshal(body, &getResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &getResponse.Value, nil
 }
 
-func (client ApiClient) DeleteGlobalRule(id string) (err error) {
-	return client.Delete("/apisix/admin/global_rules/" + id)
+// CreateGlobalRule - Creates a new global rule
+func (c *ApiClient) CreateGlobalRule(ruleID string, rule GlobalRule) (*GlobalRule, error) {
+	rb, err := json.Marshal(rule)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/apisix/admin/global_rules/%s", c.Endpoint, ruleID), strings.NewReader(string(rb)))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	creationResponse := GlobalRuleResponse{}
+	err = json.Unmarshal(body, &creationResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &creationResponse.Value, nil
+
+}
+
+// UpdateGlobalRule - Updates a global rule
+func (c *ApiClient) UpdateGlobalRule(ruleID string, rule GlobalRule) (*GlobalRule, error) {
+	rb, err := json.Marshal(rule)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/apisix/admin/global_rules/%s", c.Endpoint, ruleID), strings.NewReader(string(rb)))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	creationResponse := GlobalRuleResponse{}
+	err = json.Unmarshal(body, &creationResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &creationResponse.Value, nil
+
+}
+
+// DeleteGlobalRule - Deletes a global rule
+func (c *ApiClient) DeleteGlobalRule(ruleID string) error {
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/apisix/admin/global_rules/%s", c.Endpoint, ruleID), nil)
+	if err != nil {
+		return err
+	}
+
+	body, err := c.doRequest(req)
+	if err != nil {
+		return err
+	}
+
+	deleteResponse := DeleteResponse{}
+	err = json.Unmarshal(body, &deleteResponse)
+	if err != nil {
+		return err
+	}
+
+	if deleteResponse.Deleted != "1" {
+		return errors.New(string(body))
+	}
+
+	return nil
 }
